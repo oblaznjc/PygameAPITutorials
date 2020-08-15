73a60c2874f63ac0faadec142a06fc3441df84ad
oblaznjc <oblaznjc@rose-hulman.edu>
2020-08-15 13:02:09 -0500

    Moving Smile Drawing Step (1,2)

--- 00-MovingSmile/ExampleGameLoop.py
+++ 00-MovingSmile/ExampleGameLoop.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 
 pygame.init()
-pygame.display.set_caption("Hello World")
+pygame.display.set_caption("Hello Jacob Oblazny")
 screen = pygame.display.set_mode((640, 480))
 
 while True:

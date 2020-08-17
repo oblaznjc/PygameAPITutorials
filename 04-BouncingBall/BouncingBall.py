aa26a287a1772846c8d061e390b07e1f0a1e0fbb
oblaznjc <oblaznjc@rose-hulman.edu>
2020-08-17 11:07:46 -0500

    Moving Smile Drawing Step (1,2) Moving Smile Key Press (3) Moving Smile Key Press (4)
    DogBark (1-9)
    DogBark complete
    
    ClickInTheCircle in progress (1-7)
    ClickInTheCircle done (1-9)
    
    MikesRainyDay.py Boilerplate (1-6)
    MikesRainyDay.py Raindrop, Hero
    MikeRainyDay Cloud complete
    cleanup DONE
    
    BouncingBall.py done
    BouncingBall.py optional done

--- 04-BouncingBall/BouncingBall.py
+++ 04-BouncingBall/BouncingBall.py
@@ -1,20 +1,18 @@
 import pygame
 import sys
 import random
+import math
 
 
-# You will implement this module ENTIRELY ON YOUR OWN!
-
-# DONE: Create a Ball class.
 class Ball:
-    def __init__(self, screen, color, x, y, radius):
+    def __init__(self, screen, x, y):
         self.screen = screen
-        self.color = color
+        self.color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
         self.x = x
         self.y = y
-        self.radius = radius
-        self.speed_x = random.randint(-5, 5)
-        self.speed_y = random.randint(-5, 5)
+        self.speed_x = random.randint(-10, 10)
+        self.speed_y = random.randint(-10, 10)
+        self.radius = math.floor((200 ** 0.5) - ((self.speed_y ** 2 + self.speed_x ** 2) ** 0.5) )  # smaller = faster
 
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
@@ -28,26 +26,25 @@ class Ball:
             self.speed_y = - self.speed_y
 
     def right_left_walls(self):
-        return self.x == 0 + self.radius or self.x == self.screen.get_width() - self.radius
+        return self.x < 0 + self.radius or self.x > self.screen.get_width() - self.radius
 
     def top_bottom_walls(self):
-        return self.y == 0 + self.radius or self.y == self.screen.get_height() - self.radius
-
-
-# DONE: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
-# DONE: Methods: __init__, draw, move
+        return self.y < 0 + self.radius or self.y > self.screen.get_height() - self.radius
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((300, 300))
+    screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption('Bouncing Ball')
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
-    # DONE: Create an instance of the Ball class called ball1
-    ball = Ball(screen, (0, 0, 0), 150, 150, 10)
+    num_balls = 100
 
+    ball_list = []
+    for k in range(num_balls):
+        ball1 = Ball(screen, 500, 500)
+        ball_list.append(ball1)
 
     while True:
         for event in pygame.event.get():
@@ -57,10 +54,12 @@ def main():
         clock.tick(60)
         screen.fill(pygame.Color('gray'))
 
-        # DONE: Move the ball
-        ball.move()
-        # DONE: Draw the ball
-        ball.draw()
+        for k in range(len(ball_list)):
+            ball_list[k].move()
+            ball_list[k].draw()
+
+
+
 
         pygame.display.update()
 
@@ -69,9 +68,9 @@ main()
 
 
 # Optional challenges (if you finish and want do play a bit more): # TODO: Come back for more later!
-#   After you get 1 ball working make a few balls (ball2, ball3, etc) that start in different places.
-#   Make each ball a different color
-#   Make the screen 1000 x 800 to allow your balls more space (what needs to change?)
-#   Make the speed of each ball randomly chosen (1 to 5)
-#   After you get that working try making a list of balls to have 100 balls (use a loop)!
-#   Use random colors for each ball
+#   DONE: After you get 1 ball working make a few balls (ball2, ball3, etc) that start in different places.
+#   DONE: Make each ball a different color
+#   DONE: Make the screen 1000 x 800 to allow your balls more space (what needs to change?)
+#   DONE: Make the speed of each ball randomly chosen (1 to 5)
+#   DONE: After you get that working try making a list of balls to have 100 balls (use a loop)!
+#   DONE: Use random colors for each ball

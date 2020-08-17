d8314ccc369bb19ef64e477a59b66b2d17277aea
oblaznjc <oblaznjc@rose-hulman.edu>
2020-08-17 09:48:45 -0500

    Moving Smile Drawing Step (1,2) Moving Smile Key Press (3) Moving Smile Key Press (4)
    DogBark (1-9)
    DogBark complete
    
    ClickInTheCircle in progress (1-7)
    ClickInTheCircle done (1-9)
    
    MikesRainyDay.py Boilerplate (1-6)
    MikesRainyDay.py Raindrop, Hero
    MikeRainyDay Cloud complete
    cleanup DONE
    
    BouncingBall.py done

--- 04-BouncingBall/BouncingBall.py
+++ 04-BouncingBall/BouncingBall.py
@@ -5,9 +5,37 @@ import random
 
 # You will implement this module ENTIRELY ON YOUR OWN!
 
-# TODO: Create a Ball class.
-# TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
-# TODO: Methods: __init__, draw, move
+# DONE: Create a Ball class.
+class Ball:
+    def __init__(self, screen, color, x, y, radius):
+        self.screen = screen
+        self.color = color
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.speed_x = random.randint(-5, 5)
+        self.speed_y = random.randint(-5, 5)
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x = self.x + self.speed_x
+        self.y = self.y + self.speed_y
+        if self.right_left_walls():
+            self.speed_x = - self.speed_x
+        if self.top_bottom_walls():
+            self.speed_y = - self.speed_y
+
+    def right_left_walls(self):
+        return self.x == 0 + self.radius or self.x == self.screen.get_width() - self.radius
+
+    def top_bottom_walls(self):
+        return self.y == 0 + self.radius or self.y == self.screen.get_height() - self.radius
+
+
+# DONE: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
+# DONE: Methods: __init__, draw, move
 
 
 def main():
@@ -17,7 +45,9 @@ def main():
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
-    # TODO: Create an instance of the Ball class called ball1
+    # DONE: Create an instance of the Ball class called ball1
+    ball = Ball(screen, (0, 0, 0), 150, 150, 10)
+
 
     while True:
         for event in pygame.event.get():
@@ -27,8 +57,10 @@ def main():
         clock.tick(60)
         screen.fill(pygame.Color('gray'))
 
-        # TODO: Move the ball
-        # TODO: Draw the ball
+        # DONE: Move the ball
+        ball.move()
+        # DONE: Draw the ball
+        ball.draw()
 
         pygame.display.update()
 
@@ -36,7 +68,7 @@ def main():
 main()
 
 
-# Optional challenges (if you finish and want do play a bit more):
+# Optional challenges (if you finish and want do play a bit more): # TODO: Come back for more later!
 #   After you get 1 ball working make a few balls (ball2, ball3, etc) that start in different places.
 #   Make each ball a different color
 #   Make the screen 1000 x 800 to allow your balls more space (what needs to change?)
